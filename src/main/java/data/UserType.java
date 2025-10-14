ce80d9dfebeb32df5a40e2691e7ecafecb18452e
NickolasValentine <vovsheg.kuznetsov@mail.ru>
2025-10-14 16:37:37 +0700

    v 1.1. Stable

---
+++ src/main/java/data/UserType.java
@@ -0,0 +1,20 @@
+package data;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import data.UserType;
+import data.IntegerType;
+import data.DoubleType;
+import data.StringType;
+import data.FractionType;
+import list.SingleLinkedList;
+import util.JsonUtils;
+
+public class UserType {
+}
